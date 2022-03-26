a97bcfaf8678b745e3dca7c2b43a299ef98110af
Hohammad Hasan Abadi <Hassanabadimim@gmail.com>
2022-03-26 13:50:47 +0430

    Add Usermanagement & Campaign

---
+++ Baman.Micro.Landing/Controllers/UserManagementController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Baman.Micro.Landing.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserManagementController : ControllerBase
+    {
+    }
+}
